9feab6e72dff9accdaf1bf7bd7afd6445261d0d1
Vawnndv <dinhvanguyen810@gmail.com>
2023-05-17 17:49:10 +0700

    BE for product detail statitic

--- XPhone_Shop_TKPM/Views/TKCTSP.xaml.cs
+++ XPhone_Shop_TKPM/Views/TKCTSP.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using XPhone_Shop_TKPM.Models;
+using XPhone_Shop_TKPM.UserControls;
+using XPhone_Shop_TKPM.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +24,50 @@ namespace XPhone_Shop_TKPM.Views
     /// </summary>
     public partial class TKCTSP : Page
     {
-        public TKCTSP()
+        TKCTSPViewModel _viewModel = new TKCTSPViewModel();
+        DateTime start;
+        DateTime end;
+        int id;
+        string nameCategory;
+
+        private void refresh(DateTime start, DateTime end)
         {
+            ObservableCollection<ProductTypeStatisticModel> viewModels = new ObservableCollection<ProductTypeStatisticModel>();
+            viewModels = _viewModel.getAllProduct(start, end, id);
+
+            productChart.ItemsSource = viewModels;
+            productPieChart.ItemsSource = viewModels;
+
+            productTurnoverChart.ItemsSource = viewModels;
+            productTurnoverPieChart.ItemsSource = viewModels;
+        }
+
+        public TKCTSP(DateTime _start, DateTime _end, int _id, string _nameCategory)
+        {
+            start = _start;
+            end = _end;
+            id = _id;
+            nameCategory = _nameCategory;
+
             InitializeComponent();
+            updateDuration(start, end);
+            refresh(start, end);
+        }
+
+        private void updateDuration(DateTime start, DateTime end)
+        {
+            string s = start.ToString("dd/MM/yyyy");
+            string e = end.ToString("dd/MM/yyyy");
+
+            txtDuration.Content = "Từ ngày " + s + " đến " + e;
+            txtTitle.Content = "THỐNG KÊ SẢN PHẨM THUỘC LOẠI " + nameCategory;
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            var select = Dashboard_Admin_Sale.subMenuBTN.Children[1] as MenuButton;
+            select?.btn.Focus();
+            DataContext = new MainViewModel();
         }
     }
 }
